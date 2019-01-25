9b609956d69d217ea336ac817c4bd8db401fdb56
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2019-01-25 12:46:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,6 +12,6 @@ end
 
 #use an if/else statement pair to build the unsafe? method. It should return true if the speed is either below 40 or above 60.# 
 
-age = 1
+#age = 1
  
-age < 2 ? "baby" : "not a baby"
+age < 2 ? "baby" : "not a baby"#
